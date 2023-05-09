b1420c3359b4adb970263284a22a7410a056cfef
David Santiago Tapias <ds.tapias@uniandes.edu.co>
2023-05-08 19:09:29 -0500

    front

--- WidmyApp/widmy/widmy/views.py
+++ WidmyApp/widmy/widmy/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
 
 def healthCheck(request):
     return HttpResponse('ok')
+
+def index(request):
+    return render(request, 'inicio.html')
