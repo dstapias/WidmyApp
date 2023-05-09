1a48973a1b4c53098ff5ce75bcf5149e8edbcd1e
David Santiago Tapias <ds.tapias@uniandes.edu.co>
2023-05-08 20:03:58 -0500

    prueba

--- WidmyApp/widmy/historias/urls.py
+++ WidmyApp/widmy/historias/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+#Editando esto para wue salga en el commit
 urlpatterns = [
+       path('', views.opciones_view, name='opciones_view'),
        path('<int:documento>/', views.historia_view, name='historia_view'),
 ]
