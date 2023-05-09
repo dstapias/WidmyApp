f666109e20e97e19455337d56e0cb700011743b1
David Santiago Tapias <ds.tapias@uniandes.edu.co>
2023-05-08 20:13:42 -0500

    nuevo html

--- WidmyApp/widmy/historias/urls.py
+++ WidmyApp/widmy/historias/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 #Editando esto para wue salga en el commit
 urlpatterns = [
-       path('', views.opciones_view, name='opciones_view'),
+       path('buscar/', views.opciones_view, name='opciones_view'),
        path('<int:documento>/', views.historia_view, name='historia_view'),
 ]
